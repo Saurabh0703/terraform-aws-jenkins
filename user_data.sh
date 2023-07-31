75488c62e49878fcd6069c09a713c776cd950ace
Saurabh0703 <66934981+Saurabh0703@users.noreply.github.com>
2023-07-31 12:28:03 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,51 +1,12 @@
 #!/bin/bash
-sudo yum install java-11-amazon-corretto -y
-sudo yum install unzip -y
-sudo yum install zip -y
-
+sudo yum update –y
+sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
-sudo systemctl start jenkins
 sudo systemctl enable jenkins
+sudo systemctl start jenkins
 sudo systemctl status jenkins
-jenkins --version
-
-sudo amazon-linux-extras install docker -y
-sudo usermod -a -G docker ec2-user
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo systemctl status docker
-docker --version
-
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum install -y kubectl
-kubectl version --client --output=yaml
-
-sudo amazon-linux-extras install nginx1 -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo systemctl status nginx
-nginx -t
-nginx -version
-
-sudo yum install git -y
-git --version
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-export PATH=$PATH:/usr/local/bin
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
-chmod 700 get_helm.sh
-./get_helm.sh
-helm version --short | cut -d + -f 1
